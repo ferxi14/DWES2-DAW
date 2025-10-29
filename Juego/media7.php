59ea1e4b53c12f958ff05ad949c3ed1e89c71516
ferxi14 <fernandogr0309@gmail.com>
2025-10-29 08:46:05 +0100

    Juego7yMedia

---
+++ Juego/media7.php
@@ -0,0 +1,71 @@
+<?php
+    include "media7fun.php";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $num_cartas = $_POST['numcartas'];
+
+        $jugadores = obtenerJugadores();
+
+        $cartasTotales = array();
+        $contadorCartas = 0;
+
+        if ($num_cartas >= 1 && $num_cartas <= 10) {
+            echo "<h2>Resultado de la tirada</h2>";
+
+            foreach($jugadores as $nombre => &$jugador) {
+                echo "<table border='1'><tr><td>$nombre</td>";
+
+                $jugador['cartas'] = array();
+
+                for($i = 0; $i < $jugador['numcartas']; $i++) {
+                    $num = rand(1,10);
+                    if ($num == 8)
+                        $num = "J";
+                    if ($num == 9)
+                        $num = "Q";
+                    if ($num == 10)
+                        $num = "K";
+
+                    $letra = rand(1,4);
+                    if ($letra == 1)
+                        $letra = "C";
+                    if ($letra == 2)
+                        $letra = "D";
+                    if ($letra == 3)
+                        $letra = "P";
+                    if ($letra == 4)
+                        $letra = "T";
+
+                    $carta = (string)$num.$letra;
+
+                    if(!in_array($carta, $cartasTotales)) {
+                        array_push($cartasTotales, $carta);
+
+                        //Cartas por jugador
+						array_push($jugador['cartas'], $carta);
+						imprimirCartas($num, $letra);
+
+                        if ($num == "J" || $num == "Q" || $num == "K") {
+                            $jugador['puntos'] += 0.5;
+                        } else {
+                            $jugador['puntos'] += $num;
+                        }
+                    } else {
+                        $i--;
+                    }
+                }
+                echo "</tr></table>";
+				echo ("$nombre tiene un total de <b>{$jugador['puntos']}</b> puntos<br><br>");
+            }
+            var_dump($cartasTotales);
+			var_dump($jugadores);
+
+            $ganadores = mostrarGanador($jugadores);
+
+			darPremios($ganadores, $jugadores);
+        } else {
+			trigger_error("Error en el numero de cartas por jugador", E_USER_ERROR);
+		}
+    }
+
+?>
---
+++ Juego/media7fun.php
@@ -0,0 +1,133 @@
+<?php
+function limpiar_campo($input) {
+
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);  
+
+    return $input;
+}
+
+function obtenerJugadores() {
+    $jugadores = array();
+	$numCartas = $_POST['numcartas'];
+
+    foreach (['nombre1', 'nombre2', 'nombre3', 'nombre4'] as $jugador) {
+        if(!empty($_POST[$jugador])){
+            
+            $jugadores[limpiar_campo($_POST[$jugador])] = array(
+                                        'numcartas' => $numCartas,
+                                        'cartas' => array(),
+                                        'puntos' => 0
+                                    );
+        }
+    }
+
+	if (count($jugadores) < 2 )
+		trigger_error("Debe haber minimo 2 jugadores", E_USER_ERROR);
+
+	return $jugadores;
+}
+
+function mostrarGanador($jugadores) {
+	$puntosMax = 0;
+	$ganadores = array();
+
+	//Recorremos el array y encontramos la puntuación máxima
+	foreach ($jugadores as $nombre => $jugador) {
+		if($jugador['puntos'] <= 7.5) {
+			if ($jugador['puntos'] > $puntosMax)
+				$puntosMax = $jugador['puntos'];
+		}
+	}
+	
+	//Encontrar todos los jugadores con la puntuación máxima
+	foreach ($jugadores as $nombre => $jugador) {
+		if ($jugador['puntos'] == $puntosMax)
+			$ganadores[] = $nombre;
+	}
+
+	// Mostrar el resultado según haya empate o no
+	if (count($ganadores) > 1) {
+		echo "<h2>Hay un empate con $puntosMax puntos entre los siguientes jugadores:</h2>";
+		foreach ($ganadores as $ganador) {
+			echo "$ganador<br>";
+		}
+	} else if (count($ganadores) == 1) {
+		echo "<h2>¡El ganador es {$ganadores[0]} con $puntosMax puntos!</h2>";
+	} else {
+		echo "<h2>NO HAY GANADORES</h2>";
+	}
+
+	return $ganadores;
+}
+
+function imprimirCartas($num, $letra) {
+	echo "<td><img src='images/".$num.$letra.".PNG'></td>";
+}
+
+function darPremios($ganadores, $jugadores) {
+    $apuesta = floatval(limpiar_campo($_POST['apuesta']));
+    $bote = $apuesta;
+    $porcentajeJugadores = 0;
+    $porcentajeBanca = 0;
+
+    // Comprobar si alguien consiguió exactamente 7.5 puntos
+    $haySieteYMedia = false;
+    $encontrado = false;
+    
+    foreach ($jugadores as $jugador) {
+        if (!$encontrado && $jugador['puntos'] == 7.5) {
+            $haySieteYMedia = true;
+            $encontrado = true;
+        }
+    }
+
+    // Asignar porcentajes según haya 7.5 o no
+    if ($haySieteYMedia) {
+        $porcentajeJugadores = 0.80;
+        $porcentajeBanca = 0.20;
+    } else {
+        $porcentajeJugadores = 0.50;
+        $porcentajeBanca = 0.50;
+    }
+
+    // Calcular las cantidades
+    $totalJugadores = $bote * $porcentajeJugadores;
+    $banca = $bote * $porcentajeBanca;
+    $premioPorJugador = 0;
+
+    if (count($ganadores) > 0) {
+        $premioPorJugador = $totalJugadores / count($ganadores);
+    }
+
+    echo "<h3>Reparto de premios</h3>";
+    echo "Importe total apostado: <b>$bote €</b><br>";
+
+    if (count($ganadores) > 0) {
+        if (count($ganadores) > 1) {
+            echo "<p>Empate entre jugadores, cada uno recibe <b>$premioPorJugador €</b></p>";
+        } else {
+            echo "<p>El ganador {$ganadores[0]} recibe <b>$premioPorJugador €</b></p>";
+        }
+    } else {
+        echo "<p>No hay ganadores. La banca se queda con todo el bote.</p>";
+        $banca = $bote;
+    }
+
+    // Guardar datos en el fichero (incluye banca, pero no se muestra)
+    $contenidoFichero = "";
+    foreach ($ganadores as $ganador) {
+        $puntos = $jugadores[$ganador]['puntos'];
+        $contenidoFichero .= recogerDatos($ganador, $puntos, $premioPorJugador);
+    }
+    $contenidoFichero .= "BANCA#-#{$banca}\n";
+
+    // Escribir en el fichero
+    file_put_contents("apuestas.txt", $contenidoFichero, FILE_APPEND);
+}
+
+function recogerDatos($nombre, $puntos, $apuesta) {
+	return $nombre . "#" . $puntos . "#" . $apuesta . "\n";
+}
+?>
