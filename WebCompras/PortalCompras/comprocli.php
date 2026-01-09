dc80025658484c1958ac52dcecf1d8da83f25546
ferxi14 <fernandogr0309@gmail.com>
2026-01-09 10:12:24 +0100

    correccion

--- WebCompras/PortalCompras/comprocli.php
+++ WebCompras/PortalCompras/comprocli.php
@@ -9,26 +9,26 @@
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compra de productos</title>
-</head>
-<body>
-    <h2>Compra de productos</h2>
-    <form action="comprocli.php" method="POST">
-        <label for="producto">Selecciona el producto: </label>
-        <select name="producto" id="producto" required>
-            <?php
-                $sql = "SELECT ID_PRODUCTO, NOMBRE FROM producto";
-                imprimirOpciones($sql, 'ID_PRODUCTO', 'NOMBRE');
-            ?>
-        </select><br><br>
-        <label for="cantidad">Cantidad:</label>
-        <input type="number" name="cantidad" id="cantidad" required min="1"><br><br>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Compra de productos</title>
+    </head>
+    <body>
+        <h2>Compra de productos</h2>
+        <form action="comprocli.php" method="POST">
+            <label for="producto">Selecciona el producto: </label>
+            <select name="producto" id="producto" required>
+                <?php
+                    $sql = "SELECT ID_PRODUCTO, NOMBRE FROM producto";
+                    imprimirOpciones($sql, 'ID_PRODUCTO', 'NOMBRE');
+                ?>
+            </select><br><br>
+            <label for="cantidad">Cantidad:</label>
+            <input type="number" name="cantidad" id="cantidad" required min="1"><br><br>
 
-        <input type="submit" value="Añadir al carrito">
-    </form>
-    <h3>Carrito</h3>
-    
+            <input type="submit" value="Añadir al carrito">
+        </form>
+        <h3>Carrito</h3>
+    </body>
 </html>
